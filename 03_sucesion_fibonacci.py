3cf09f89e0fc6b253730b7269a7725ecaade84c9
Joseph Code <pepet_21@hotmail.com>
2024-10-10 20:40:16 +0200

    Ejercicio 3 resuelto

---
+++ 03_sucesion_fibonacci.py
@@ -0,0 +1,18 @@
+"""
+Escribe un programa que imprima los 50 primeros números de la sucesión
+de Fibonacci empezando en 0.
+- La serie Fibonacci se compone por una sucesión de números en
+  la que el siguiente siempre es la suma de los dos anteriores.
+  0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_fib = fib_sequence[i-1] + fib_sequence[i-2]
+        fib_sequence.append(next_fib)
+    return fib_sequence
+
+# Imprimir los primeros 50 números de la sucesión de Fibonacci
+fibonacci_50 = fibonacci(50)
+print(fibonacci_50)

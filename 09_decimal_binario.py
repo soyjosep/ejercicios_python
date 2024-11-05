d4dc8dd7f19d4199f77ce025df7f30e92bfea1a2
Joseph Code <pepet_21@hotmail.com>
2024-11-05 18:52:49 +0100

    ejercicio 09 v1.0.0

--- 09_decimal_binario.py
+++ 09_decimal_binario.py
@@ -4,18 +4,36 @@ decimal a binario sin utilizar funciones propias del lenguaje que lo hagan direc
 """
 
 def decimal_a_binario(numero):
+    """ 
+    Convierte un número decimal a su representación en binario.
+
+    Parámetros:
+    numero (int): El número decimal a convertir.
+
+    Retorna:
+    str: La representación binaria del número.
+    """
     if numero == 0:
         return "0"
     
     binario = ""
     while numero > 0:
         residuo = numero % 2
-        binario = str(residuo) + binario  # Agregar el residuo al inicio de la cadena
-        numero = numero // 2  # Actualizar el número dividiéndolo por 2
-    
+        binario = str(residuo) + binario
+        numero = numero // 2
+
     return binario
 
-# Ejemplo de uso
-numero_decimal = int(input("Ingresa un número decimal: "))
-binario = decimal_a_binario(numero_decimal)
-print(f"El número {numero_decimal} en binario es: {binario}")
+def main ():
+    try:
+        numero = int(input("Ingrese un número decimal para convertir a binario: "))
+        if numero < 0:
+            print("Por favor, ingrese un número positivo.")
+        else:
+            binario = decimal_a_binario(numero)
+            print(f"Lan respresentación binaria de {numero} es: {binario}")
+    except ValueError:
+        print("Entrada no válida. Por favor, ingrese un número entero.")
+
+if __name__ == "__main__":
+    main()
